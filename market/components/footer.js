20e7e5e6d487afb57b1139053476e8af48c30b83
Jacob Ford <j@cobford.com>
2022-08-23 00:11:11 -0400

    Market: fix nonextensible object

--- market/components/footer.js
+++ market/components/footer.js
@@ -1,8 +1,9 @@
 import styles from '../styles/Home.module.css'
 
-const BrietFooter = <footer className={styles.footer}>
-  <a href="//server.briet.app">An early BRIET prototype</a>
-</footer>
+const BrietFooter = () =>
+  <footer className={styles.footer}>
+    <a href="//server.briet.app">An early BRIET prototype</a>
+  </footer>
 
 BrietFooter.displayName = 'BrietFooter'
 
