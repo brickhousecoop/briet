6075eb7a924e424281b2865e8db0612c2c4425b3
Jacob Ford <j@cobford.com>
2023-04-08 01:59:09 -0400

    Add Vercel Analytics

--- apps/jacket/app/layout.tsx
+++ apps/jacket/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics as VercelAnalytics } from '@vercel/analytics/react'
+
 import './globals.css'
 
 export const metadata = {
@@ -12,7 +14,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <VercelAnalytics />
+      </body>
     </html>
   )
 }
