01f6af5e9f9983f19d8a6fc7deb2e1e14ac3bce7
Jacob Ford <j@cobford.com>
2023-04-19 12:55:25 -0400

    Add BH newsletter subscribe to About

--- apps/market/pages/about.js
+++ apps/market/pages/about.js
@@ -39,6 +39,29 @@ const BrietAboutPage = () => {
         <p>We seek charitable funding support to maintain the new BRIET platform, expand its catalog of works for libraries and schools to acquire, and develop its open technologies. The Brick House Cooperative’s allied organization, the Participatory Politics Foundation (PPF), is a 501(c)(3) nonprofit organization founded in 2009 to increase civic engagement. Please get in touch to support our efforts to defend press freedom, libraries, and the digital future. Contact: David Moore and Maria Bustillos, Project Managers</p>
 
         <p>Read more about <span className="logo">BRIET</span>: our pioneering <Link href="https://www.thenation.com/article/culture/libraries-digital-publishing-ebooks/">sale</Link> of one ebook to the Internet Archive’s Open Library in 2021, and our cooperative’s work <Link href="https://popula.com/2022/01/12/buy-this-book-from-the-brick-house-cooperative/">defending library rights</Link>.</p>
+
+        <link href="//cdn-images.mailchimp.com/embedcode/classic-071822.css" rel="stylesheet" type="text/css" />
+        <div id="mc_embed_signup">
+            <form action="https://house.us17.list-manage.com/subscribe/post?u=32a22cc8bacf327fb3bb5066e&amp;id=7757e9bd40&amp;f_id=006b5ae0f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                <div id="mc_embed_signup_scroll">
+                <h2>The Brick House Newsletter is Good</h2>
+                <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+        <div class="mc-field-group">
+          <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+        </label>
+          <input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required />
+          <span id="mce-EMAIL-HELPERTEXT" class="helper_text"></span>
+        </div>
+        <div hidden="true"><input type="hidden" name="tags" value="6561264" /></div>
+          <div id="mce-responses" class="clear">
+            <div class="response" id="mce-error-response" style={{display: 'none'}}></div>
+            <div class="response" id="mce-success-response" style={{display: 'none'}}></div>
+          </div>
+            <div style={{position: 'absolute', left: '-5000px'}} aria-hidden="true"><input type="text" name="b_32a22cc8bacf327fb3bb5066e_7757e9bd40" tabindex="-1" value="" /></div>
+            <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button" /></div>
+            </div>
+        </form>
+        </div>
       </main>
       <Footer/>
     </div>
