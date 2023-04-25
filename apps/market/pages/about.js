1b30cc353a6b0fc4d10e3372e6d9fe75ad9cf971
Jacob Ford <j@cobford.com>
2023-04-25 16:34:46 -0400

    class -> className

--- apps/market/pages/about.js
+++ apps/market/pages/about.js
@@ -42,23 +42,23 @@ const BrietAboutPage = () => {
 
         <link href="//cdn-images.mailchimp.com/embedcode/classic-071822.css" rel="stylesheet" type="text/css" />
         <div id="mc_embed_signup">
-            <form action="https://house.us17.list-manage.com/subscribe/post?u=32a22cc8bacf327fb3bb5066e&amp;id=7757e9bd40&amp;f_id=006b5ae0f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+            <form action="https://house.us17.list-manage.com/subscribe/post?u=32a22cc8bacf327fb3bb5066e&amp;id=7757e9bd40&amp;f_id=006b5ae0f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" novalidate>
                 <div id="mc_embed_signup_scroll">
                 <h2>The Brick House Newsletter is Good</h2>
-                <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-        <div class="mc-field-group">
-          <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+                <div className="indicates-required"><span className="asterisk">*</span> indicates required</div>
+        <div className="mc-field-group">
+          <label for="mce-EMAIL">Email Address  <span className="asterisk">*</span>
         </label>
-          <input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required />
-          <span id="mce-EMAIL-HELPERTEXT" class="helper_text"></span>
+          <input type="email" name="EMAIL" className="required email" id="mce-EMAIL" required />
+          <span id="mce-EMAIL-HELPERTEXT" className="helper_text"></span>
         </div>
         <div hidden="true"><input type="hidden" name="tags" value="6561264" /></div>
-          <div id="mce-responses" class="clear">
-            <div class="response" id="mce-error-response" style={{display: 'none'}}></div>
-            <div class="response" id="mce-success-response" style={{display: 'none'}}></div>
+          <div id="mce-responses" className="clear">
+            <div className="response" id="mce-error-response" style={{display: 'none'}}></div>
+            <div className="response" id="mce-success-response" style={{display: 'none'}}></div>
           </div>
             <div style={{position: 'absolute', left: '-5000px'}} aria-hidden="true"><input type="text" name="b_32a22cc8bacf327fb3bb5066e_7757e9bd40" tabindex="-1" value="" /></div>
-            <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button" /></div>
+            <div className="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button" /></div>
             </div>
         </form>
         </div>
