ca1cab9be913e8b5731983a0306bba2c4b4e5b11
Jacob Ford <j@cobford.com>
2023-05-11 13:54:09 -0400

    Add taglines

--- apps/jacket/app/page.tsx
+++ apps/jacket/app/page.tsx
@@ -50,6 +50,7 @@ export default function Home() {
           <h2 className={inter.className}>
             Tagger <span>-&gt;</span>
           </h2>
+          <p className={styles.tag}>BY INVITATION ONLY</p>
           <p className={inter.className}>
             Authors upload and tag their e-books with library-friendly metadata
           </p>
@@ -62,8 +63,9 @@ export default function Home() {
           <h2 className={inter.className}>
             Server <span>-&gt;</span>
           </h2>
+          <p className={styles.tag}>FOR MACHINES ONLY</p>
           <p className={inter.className}>
-            We serve an open searchable feed of books, powered by BookServer
+            Run a catalog? Ingest our open feed of books, powered by BookServer
           </p>
         </a>
 
@@ -74,6 +76,7 @@ export default function Home() {
           <h2 className={inter.className}>
             Market <span>-&gt;</span>
           </h2>
+          <p className={styles.tag}>OPEN TO ALL LIBRARIANS</p>
           <p className={inter.className}>
             Librarians browse for e-books and easily add them to their catalog
           </p>
