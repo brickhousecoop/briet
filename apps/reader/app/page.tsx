88abc44b8ea90b2b95028ccbdebb4d546da854e1
Jacob Ford <j@cobford.com>
2024-03-21 15:57:09 -0400

    Various experiments in (failing to) import BookReader

--- apps/reader/app/page.tsx
+++ apps/reader/app/page.tsx
@@ -16,6 +16,7 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             By{" "}
+            <div id="BookReader"></div>
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
