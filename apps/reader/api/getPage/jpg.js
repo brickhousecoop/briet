a135f35066a9265521f746f3d942b88895e400d1
Jacob Ford <j@cobford.com>
2024-03-28 16:39:57 -0400

    Beginnings of an endpoint

---
+++ apps/reader/api/getPage/jpg.js
@@ -0,0 +1,35 @@
+var pdf2img = require('pdf-img-convert');
+
+export async function GET(request) {
+  // const { fileId, index } = request.nextUrl.searchParams
+  const fileId = '0e7fab901df482ab46a641a54b487f9c6fea41e2';
+  const index = 1;
+
+  const pdfUrl = `https://cdn.sanity.io/files/3lm68n5v/production/${fileId}.pdf`
+
+  const image = await pdfPageToJpg2000({
+    pdfUrl: pdfUrl,
+    pageNumber: index,
+  })
+
+  console.log(typeof image)
+
+  const headers = new Headers();
+  headers.set('Content-Type', 'image/jpg')
+
+  return new Response(image, { headers })
+}
+
+async function pdfPageToJpg2000({pdfUrl, pageNumber}={}) {
+  const convertOptions = {
+    width: 800, // TODO: hardcoded in BrietBookLoader.js
+    height: 1200, // TODO: hardcoded in BrietBookLoader.js
+    page_numbers: [pageNumber],
+    scale: 2.0,
+  };
+
+  const imageArray = await pdf2img.convert(pdfUrl, convertOptions)
+  const imageData = imageArray[0]
+
+  return imageData
+}
