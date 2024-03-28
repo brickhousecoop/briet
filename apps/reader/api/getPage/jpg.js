62e5c5d6af51b1d832c7fd578b658c3e573c3c45
Jacob Ford <j@cobford.com>
2024-03-28 17:32:41 -0400

    Back to the old new API we go, my goodness

--- apps/reader/api/getPage/jpg.js
+++ apps/reader/api/getPage/jpg.js
@@ -1,7 +1,13 @@
 var pdf2img = require('pdf-img-convert');
 
-export default async (request, response) => {
-  const { sanityFileId, index } = request.query
+export async function GET(request) {
+  const url = new URL(request.url)
+  const searchParams = new URLSearchParams(url.search)
+
+  const sanityFileId = searchParams.get('sanityFileId')
+  const index = parseInt(searchParams.get('index'), 10);
+
+  console.log('sanityFileId', sanityFileId)
 
   const pdfUrl = `https://cdn.sanity.io/files/3lm68n5v/production/${sanityFileId}.pdf`
 
@@ -9,9 +15,13 @@ export default async (request, response) => {
     pdfUrl: pdfUrl,
     pageNumber: index,
   })
+https://cdn.sanity.io/files/3lm68n5v/production/0e7fab901df482ab46a641a54b487f9c6fea41e2.pdf
+  console.log(typeof image)
 
-  response.setHeader('Content-Type', 'image/jpeg');
-  response.send(image)
+  const headers = new Headers();
+  headers.set('Content-Type', 'image/jpg')
+
+  return new Response(image, { headers })
 }
 
 async function pdfPageToJpg2000({pdfUrl, pageNumber}={}) {
