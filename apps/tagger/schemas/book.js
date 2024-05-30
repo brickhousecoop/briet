8c097c6bb67bf9135532c1854bead913e8d04025
Jacob Ford <j@cobford.com>
2024-05-30 13:32:57 -0400

    Reword free book warning

--- apps/tagger/schemas/book.js
+++ apps/tagger/schemas/book.js
@@ -90,7 +90,7 @@ export default defineType({
       type: 'number',
       validation: Rule => [
         Rule.required().positive().precision(2),
-        Rule.custom(price => price === 0 ? 'Free book! But just double checking--$0 books will be available for immediate download by the public.' : true).warning(),
+        Rule.custom(price => price === 0 ? 'Just double checking you meant to make this book free! $0 books will be available for immediate download by the public.' : true).warning(),
       ],
     }),
     defineField({
