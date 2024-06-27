c3ea163f1bc31e677e5e293a48f736bb3810a238
Jacob Ford <j@cobford.com>
2024-06-27 17:28:02 -0400

    nvm I broke it, now fixed

--- apps/market/pages/_app.tsx
+++ apps/market/pages/_app.tsx
@@ -6,16 +6,18 @@ import '@styles/globals.css'
 import Script, { ScriptProps } from 'next/script'
 
 function App({ Component, pageProps }: AppProps) {
-  <>
-    <Script
-      src="https://cdn.usefathom.com/script.js"
-      data-site={process.env.NEXT_PUBLIC_FATHOM_SITEID}
-      strategy="afterInteractive"
-    />
-    <Component {...pageProps} />
-    <VercelAnalytics />
-    <SpeedInsights />
-  </>
+  return (
+    <>
+      <Script
+        src="https://cdn.usefathom.com/script.js"
+        data-site={process.env.NEXT_PUBLIC_FATHOM_SITEID}
+        strategy="afterInteractive"
+      />
+      <Component {...pageProps} />
+      <VercelAnalytics />
+      <SpeedInsights />
+    </>
+  )
 }
 
 export default App
