a746d2a1188f78c5e53280f2081eff4cb91945d8
Jacob Ford <j@cobford.com>
2024-06-27 17:23:56 -0400

    Fixed it

--- apps/market/pages/_app.tsx
+++ apps/market/pages/_app.tsx
@@ -3,21 +3,19 @@ import { Analytics as VercelAnalytics } from '@vercel/analytics/react'
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import '@styles/globals.css'
-import Script from 'next/script'
+import Script, { ScriptProps } from 'next/script'
 
 function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Script
-        src="https://cdn.usefathom.com/script.js"
-        data-site={process.env.NEXT_PUBLIC_FATHOM_SITEID}
-        strategy="afterInteractive"
-      />
-      <Component {...pageProps} />
-      <VercelAnalytics />
-      <SpeedInsights />
-    </>
-  )
+  <>
+    <Script
+      src="https://cdn.usefathom.com/script.js"
+      data-site={process.env.NEXT_PUBLIC_FATHOM_SITEID}
+      strategy="afterInteractive"
+    />
+    <Component {...pageProps} />
+    <VercelAnalytics />
+    <SpeedInsights />
+  </>
 }
 
 export default App
