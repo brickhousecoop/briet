8c8b26ee15da251ea9933906361a914e370cd7bf
Jacob Ford <j@cobford.com>
2024-07-25 15:56:55 -0400

    Require alt text

--- apps/tagger/schemas/pageContent.js
+++ apps/tagger/schemas/pageContent.js
@@ -3,7 +3,21 @@ export default {
   title: 'Content',
   type: 'array',
   of: [
-    {type: 'block'},
-    {type: 'image'},
+    {
+      type: 'block',
+    },
+    {
+      type: 'image',
+      fields: [
+      {
+        title: 'Alt Text (description)',
+        name: 'alt',
+        type: 'string',
+        validation: Rule => Rule.required(),
+        options: {
+          isHighlighted: true,
+        },
+      }]
+    },
   ],
 }
