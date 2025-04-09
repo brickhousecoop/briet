6c25680277ec600be61e75d577f49dc999b82e2e
Jacob Ford <j@cobford.com>
2025-04-09 13:41:50 -0400

    market: upgrade Clerk

--- apps/market/middleware.ts
+++ apps/market/middleware.ts
@@ -13,8 +13,8 @@ const isPublicRoute = createRouteMatcher([
   '/account/sign-up(.*)',
 ])
 
-export default clerkMiddleware((auth, req) => {
+export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
-    auth().protect()
+    await auth.protect()
   }
 })
